39630e3846407b229d50ec96283f0623c552df4d
aldidaim21 <aldidaimfauzan15@gmail.com>
2025-04-04 17:07:00 +0700

    data

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 #show data
 def load_data():
-    df= pd.read_csv("dataset\covid_19_indonesia_time_series_all.csv")
+    df= pd.read_csv("covid_19_indonesia_time_series_all.csv")
     return df
 
 def show_data():
