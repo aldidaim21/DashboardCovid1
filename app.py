e849d5141a044bc1a8e8da517c17172ce716b0a5
aldidaim21 <aldidaimfauzan15@gmail.com>
2025-04-14 12:25:43 +0700

    addbar

--- app.py
+++ app.py
@@ -13,19 +13,25 @@ menu = st.sidebar.radio("Pilih Halaman", ["Home", "Halaman Data"])
 # Halaman HOME
 if menu == "Home": 
     judul()
-    # Pilih tahun
-    year = select_year()
-    # Load & filter data
     df = load_data()
-    df_filtered = filter_data(df, year)
+    year = select_year()
+    locations = select_location(df)
+
+    # Filter data
+    df_filtered = filter_data(df, year, locations)
     kolom(df_filtered)
     pie_chart1(df_filtered)
 
-# Halaman DATAx``
+    #barchart
+    bar_chart1(df_filtered)
+    bar_chart2(df_filtered)
+
+
+# Halaman DATA
 elif menu == "Halaman Data":
     judul()
-    year = select_year()
-    # Load & filter data
     df = load_data()
-    df_filtered = filter_data(df, year)
+    year = select_year()
+    locations = select_location(df)
+    df_filtered = filter_data(df, year, locations)
     show_data(df_filtered)
--- data.py
+++ data.py
@@ -6,12 +6,15 @@ import plotly.express as px
 #show data
 def load_data():
     df= pd.read_csv("covid_19_indonesia_time_series_all.csv")
+    df = df[df["Location"] != "Indonesia"]
     return df
 
 # Filter data berdasarkan tahun (optional)
-def filter_data(df, year=None):
+def filter_data(df, year=None, locations=None):
     if year:
         df = df[df['Date'].astype(str).str.contains(str(year))]
+    if locations:
+        df = df[df['Location'].isin(locations)]
     return df
 
 def select_year():
@@ -19,6 +22,14 @@ def select_year():
         "Pilih Tahun 📅",
         options=[None, 2020, 2021, 2022],
         format_func=lambda x: "Semua Tahun" if x is None else x
+    )
+
+def select_location(df):
+    locations = sorted(df['Location'].unique())
+    return st.sidebar.multiselect(
+        "Pilih Provinsi 📍",
+        options=locations,
+        default=locations 
     )
 
 def show_data(df):
@@ -29,18 +40,18 @@ def show_data(df):
     
 # Fungsi untuk total kasus
 def total_case(df):    
-    total_kasus = df['Total Cases'].sum()
-    return total_kasus
+    total_kasus = df.sort_values('Date').groupby('Location', as_index=False).last()
+    return total_kasus['Total Cases'].sum()
 
 # Fungsi untuk total kematian
 def total_death(df):
-    total_mati = df['Total Deaths'].sum()
-    return total_mati
+    total_mati = df.sort_values('Date').groupby('Location', as_index=False).last()
+    return total_mati['Total Deaths'].sum()
 
 # Fungsi untuk total sembuh
 def total_recovery(df):
-    total_sembuh = df['Total Recovered'].sum()
-    return total_sembuh
+    total_sembuh = df.sort_values('Date').groupby('Location', as_index=False).last()
+    return total_sembuh['Total Recovered'].sum()
 
 # Tampilkan scoreboard/metrik dalam 3 kolom
 def kolom(df):
@@ -50,9 +61,9 @@ def kolom(df):
 
     col1, col2, col3 = st.columns(3)
 
-    col1.metric(label="🦠 Total Kasus", value=f"{kasus/1000:.1f}K",border=True)
-    col2.metric(label="💀 Total Kematian", value=f"{kematian/1000:.1f}K", border=True)
-    col3.metric(label="💚 Total Sembuh", value=f"{sembuh/1000:.1f}K", border=True)
+    col1.metric(label="🦠 Total Kasus", value=kasus,border=True)
+    col2.metric(label="💀 Total Kematian", value=kematian, border=True)
+    col3.metric(label="💚 Total Sembuh", value=sembuh, border=True)
 
 def pie_chart1(df):
     # Ambil data total kematian dan sembuh
@@ -76,4 +87,49 @@ def pie_chart1(df):
     )
 
     # Tampilkan chart di Streamlit
+    st.plotly_chart(fig, use_container_width=True)
+
+
+def bar_chart1(df):
+    # Ambil data terakhir per provinsi (group by Location ambil baris terakhir)
+    df_last = df.sort_values('Date').groupby('Location', as_index=False).last()
+
+    # Ambil 5 provinsi dengan kematian terbanyak
+    top5 = df_last.nlargest(5, 'Total Deaths')
+
+    # Buat bar chart
+    fig = px.bar(
+        top5,
+        x='Location',
+        y='Total Deaths',
+        color='Total Deaths',
+        color_continuous_scale='Reds',
+        title='🔝 5 Provinsi dengan Kematian Tertinggi',
+        labels={'Total Deaths': 'Total Kematian', 'Location': 'Provinsi'}
+    )
+
+    fig.update_layout(xaxis_title='Provinsi', yaxis_title='Total Kematian', title_x=0.5)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+def bar_chart2(df):
+      # Ambil data terakhir per provinsi (group by Location ambil baris terakhir)
+    df_last = df.sort_values('Date').groupby('Location', as_index=False).last()
+
+    # Ambil 5 provinsi dengan kematian terbanyak
+    top5 = df_last.nlargest(5, 'Total Recovered')
+
+    # Buat bar chart
+    fig = px.bar(
+        top5,
+        x='Location',
+        y='Total Recovered',
+        color='Total Recovered',
+        color_continuous_scale='greens',
+        title='🔝 5 Provinsi dengan Kesembuhan Tertinggi',
+        labels={'Total Recovered': 'Total Kesembuhan', 'Location': 'Provinsi'}
+    )
+
+    fig.update_layout(xaxis_title='Provinsi', yaxis_title='Total Kesembuhan', title_x=0.5)
+
     st.plotly_chart(fig, use_container_width=True)
